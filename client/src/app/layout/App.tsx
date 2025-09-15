51b57fd48f8a1b07eafe3ad1c11aabcec1a6850f
Clive Harding <mufasa8005@gmail.com>
2025-09-15 10:52:16 +0200

    End of Section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+
+import Catalog from "../../features/catalog/Catalog";
+import type { Product } from "../models/product";
+import NavBar from "./NavBar";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: darkMode ? "#121212" : "#eaeaea",
+        }}
+      >
+        <Container maxWidth="xl" sx={{ mt: 14 }}>
+          <Catalog products={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
